9ae98350b2b04beee24895da1f61132628af569f
DELL <samsirathsylla@gmail.com>
2025-07-15 19:06:04 +0200

    mis à jour

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import Accueil from "./Components/Accueil.jsx";
 import Footer from "./Components/Footer.jsx";
 import Projets from "./Components/Projets.jsx";
 import Contact from './Components/Contact.jsx';
-import Apropos from './Components/Apropos.jsx';
+import Apropos from './Components/Apropos.jsx'; 
 import Navbar from './Components/Navbar.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
